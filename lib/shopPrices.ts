b08cabd74f449504866d55881359137a4dbdaec1
Pepe Ortiz Roldán <99665744+madgIitch@users.noreply.github.com>
2026-01-14 16:05:21 +0100

    ajuste de precios

--- lib/shopPrices.ts
+++ lib/shopPrices.ts
@@ -1,18 +1,50 @@
-export const SHOP_PRICES: Record<string, number> = {
+﻿export const SHOP_PRICES: Record<string, number> = {
   // Banderas
+  "banderas/Andalucía Independentista.png": 8,
+  "banderas/Bilbao Anarkista.png": 7,
   "banderas/Ikurriña.png": 6,
+  "banderas/Navarra.png": 6,
+  "banderas/Palestina.png": 7,
   "banderas/Presoak Etxera.png": 8,
 
-  // carteles
+  // Carteles
+  "carteles/Basurto.png": 9,
+  "carteles/BBK.png": 9,
+  "carteles/Carrero Blanco.png": 9,
+  "carteles/EMK.png": 9,
+  "carteles/GKS.png": 9,
+  "carteles/Iraultza.png": 9,
   "carteles/Marijaia.png": 9,
   "carteles/OKUPA.png": 10,
 
-  // vinilos
+  // Vinilos
   "vinilos/el estado de las cosas.png": 10,
   "vinilos/EZORREGATIK X BERPIZKUNDEA.png": 12,
   "vinilos/kortatu.png": 10,
   "vinilos/si abro los ojos no es real.png": 12,
   "vinilos/turboviolencia.png": 14,
   "vinilos/vaciador.png": 10,
+
+  // Alfombras
+  "alfombras/Alfombra azulita.png": 8,
+  "alfombras/Alfombra con flores verdes.png": 10,
+  "alfombras/Alfombra con flores.png": 9,
+  "alfombras/Alfombra rosita.png": 8,
+  "alfombras/Alfombra verdecita.png": 8,
+  "alfombras/Alfomrriña.png": 7,
+  "alfombras/Huella Violeta.png": 10,
+  "alfombras/Huella.png": 9,
+
+  // Muebles
+  "muebles/Estantería Madera Premium.png": 22,
+  "muebles/Estantería Madera.png": 12,
+  "muebles/Estantería Moderna.png": 16,
+  "muebles/Mesa básica.png": 12,
+  "muebles/Mesa deluxe.png": 24,
+  "muebles/Mesa media.png": 16,
+  "muebles/Mesa premium.png": 20,
+  "muebles/Sillon básico.png": 14,
+  "muebles/Sillon medio.png": 18,
+  "muebles/Sillon premium.png": 24,
 
 };
